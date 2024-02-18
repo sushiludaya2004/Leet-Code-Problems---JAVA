7006c29c0a887549b8a65415206ea1db3c8b43f7
Sushil <127210875+sushiludaya2004@users.noreply.github.com>
2024-02-18 15:33:39 +0530

    Create 1108_ip.java

---
+++ 1108_ip.java
@@ -0,0 +1,6 @@
+//https://leetcode.com/problems/defanging-an-ip-address/description/
+class Solution {
+    public String defangIPaddr(String address) {
+        return address.replace(".", "[.]");
+    }
+}
