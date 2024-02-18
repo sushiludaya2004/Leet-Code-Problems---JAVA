969cdb749ffe840ea0c8087d79270632c8b852a1
Sushil <127210875+sushiludaya2004@users.noreply.github.com>
2024-02-18 15:33:06 +0530

    Create 1662_equalornot.java

---
+++ 1662_equalornot.java
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/description/
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        String w1 = "";
+        String w2 = "";
+        for(String i : word1) w1 += i;
+        for(String i : word2) w2 += i;
+
+        return w1.equals(w2);
+    }
+}
